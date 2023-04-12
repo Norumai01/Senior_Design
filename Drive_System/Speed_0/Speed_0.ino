779e2aad64e14e012284a2de2f88372cecec8625
Norumai01 <johnnydinhnguyen2001@gmail.com>
2023-04-12 17:12:34 -0400

    Update Speed_0.ino

--- Drive_System/Speed_0/Speed_0.ino
+++ Drive_System/Speed_0/Speed_0.ino
@@ -5,9 +5,11 @@
 // Initate Nunchuck
 Accessory nunchuck;
 // Configure the motor driver.
-CytronMD motor(PWM_DIR, 3, 4);  // PWM = Pin 3, DIR = Pin 4.
+CytronMD motorR(PWM_DIR, 3, 4);  // PWM = Pin 3, DIR = Pin 4.
+CytronMD motorL(PWM_DIR, 5, 6);  // PWM = Pin 5, DIR = Pin 6.
 
 bool motor_dir = false;
+bool e_brake = false; 
 int PWM_Speed = 0;
 
 void setup() {
@@ -19,7 +21,9 @@ void setup() {
 	}
 
   delay(2000);  // Startup Loading of 2 seconds.
-  motor.setspeed(0);  // Set all motor speed to zero at startup.
+  // Set all motor speed to zero at startup.
+  motorR.setspeed(0);  
+  motorL.setspeed(0);
 }
 
 void loop() {
@@ -28,7 +32,7 @@ void loop() {
   int x_axis = nunchuck.getJoyX();
   int y_axis = nunchuck.getJoyY(); 
   bool c_but = nunchuck.getButtonC();
-  bool z_But = nunchuck.getButtonZ();
+  bool z_but = nunchuck.getButtonZ();
   
   // Change motor direction (forward/reverse), if C-button is pressed.
   // Probably won't be needed for Speed 0. 
@@ -37,20 +41,37 @@ void loop() {
     Serial.print("Motor direction is in reverse.");
     Serial.println();
   }
-  else {
+  if (!c_but) {
     motor_dir = false;
     Serial.print("Motor direction is in forward.");
     Serial.println();
   }
 
-  // Joystick bypass extreme y-axis, stops the vehicle. 
+  // Joystick bypass extreme y-axis range, stops the vehicle. 
   // Emergency electrical brake. 
   if (y_axis > 215) || y_axis < 35) {
     PWM_Speed = 0;
-    motor.setspeed(PWM_Speed);
+    // Insert code this line. Reset speed flag to 0.
+    motorR.setspeed(PWM_Speed);
+    motorL.setspeed(PWM_Speed);
+    Serial.print("E-Brake is on.");
+    Serial.println();
+    e_brake = true; 
+  }
+  else {
+    // Insert code this line. Set PWM Speed 
+    e_brake = false;
   } 
 
   // Turning right.
+  if (x_axis > 140 && !(e_brake)) {
+
+  }
+
+  // Turning left.
+  if (x_axis < 110 && !(e_brake)) {
+    
+  }
   
 
 
